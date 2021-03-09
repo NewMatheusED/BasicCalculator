1ea13080c6ecba0adbee0cb5dc67b757231f87c5
Matheus <matheus081105@gmail.com>
2021-03-08 21:06:45 -0300

    .

---
+++ app.js
@@ -0,0 +1,23 @@
+    $('input[type=button]').mousedown(function(){
+        $(this).css({'color': '#3498db', 'transform': 'scale(0.9)', 'box-shadow': 'inset 2px 2px 5px rgba(0, 0, 0, 0.5), inset -3px -3px 7px rgba(255, 255, 255, 0.05);' })
+    })
+
+    $('html').mouseup(function(){
+        $('input[type=button]').css({'color': '#b3b3b3', 'transform': 'scale(1)', 'box-shadow': 'inset 2px 2px 5px rgba(0, 0, 0, 0.5), inset -3px -3px 7px rgba(255, 255, 255, 0.05);'})
+    })
+
+    function setValue(currentValue){
+        form.display.value = form.display.value + currentValue  
+    }
+
+    function calc(){
+        if(form.display.value === '') {
+            alert("Digite sua conta antes")
+        }else {
+            form.display.value = eval(form.display.value)
+        }
+    }
+    
+    $('input[name=equals]').dblclick(function(){
+        form.display.value = ''
+    })
