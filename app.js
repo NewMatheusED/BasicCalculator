e17772d3c58293ee42e9861895789501c16644ee
Matheus <matheus081105@gmail.com>
2021-03-18 12:45:07 -0300

    .

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-    $('input[type=button]').mousedown(function(){
+    $('input[type=button]').mousedown(function() {
         $(this).css({'color': '#3498db', 'transform': 'scale(0.9)', 'box-shadow': 'inset 2px 2px 5px rgba(0, 0, 0, 0.5), inset -3px -3px 7px rgba(255, 255, 255, 0.05);' })
     })
 
-    $('html').mouseup(function(){
+    $('html').mouseup(function() {
         $('input[type=button]').css({'color': '#b3b3b3', 'transform': 'scale(1)', 'box-shadow': 'inset 2px 2px 5px rgba(0, 0, 0, 0.5), inset -3px -3px 7px rgba(255, 255, 255, 0.05);'})
     })
-
-    function setValue(currentValue){
+ 
+    function setValue(currentValue) {
         form.display.value = form.display.value + currentValue  
     }
 
-    function calc(){
+    function calc() {
         if(form.display.value === '') {
             alert("Digite sua conta antes")
         }else {
@@ -18,6 +18,11 @@
         }
     }
     
-    function cleanAll(){
+    function cleanAll() {
         form.display.value = ''
+    }
+
+    function Delete() {
+        form.display.value = form.display.value.substr(0, form.display.value.length - 1)
+        
     }
